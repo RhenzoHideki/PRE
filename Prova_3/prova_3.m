ac5a9feede43bab9c6e4f2039eb4f5b26fc5b5b7
RhenzoHideki <rhenzo.hsk@aluno.ifsc.edu.br>
2023-10-02 18:03:27 -0300

    Adição das questoes b c d e f simuladas

--- Prova_3/prova_3.m
+++ Prova_3/prova_3.m
@@ -11,11 +11,50 @@
 close all; clear all ; clc;
 
 N = 100000;
+X = 20*rand(1 ,N) - 10;
 
-%Gerar X e Y
-X = randi([-10 10], 1 ,N);
+for i = 1 : N
+  Y(i) = rand()*(20 - abs(X(i))) ;
+end
 
 figure; hold on ; grid on;
+scatter(X(1:5000),Y(1:5000));
+axis('square');
 
-plot(X/N);
+##(b) Determine Pr[X ≥ Y ].a
+
+PrX_maior_igual_Y_sim = mean(X >= Y)
+PrX_maior_igual_Y_teo = 1/6
+
+% c) PDF marginal de Y
+
+y = -25 : 0.5 : 25;
+
+histY = hist(Y, y);
+pdfY_sim = histY / trapz(y, histY);
+%pdfY_teo ;
+
+figure
+subplot(3, 1, 1); grid on; hold on;
+bar(y, pdfY_sim, 'y');
+
+% d) CDF marginal de Y
+
+cdfY_sim = cumsum(histY) / N;
+
+subplot(3, 1, 2); grid on; hold on;
+plot(y, cdfY_sim, 'g', 'LineWidth', 4);
+
+% e) PDF condicional de Y dado que X = 5.
+
+idx = (4 <= X) & (X <= 6);
+histY_condX = hist(Y(idx), y);
+pdfY_condX_sim = histY_condX / trapz(y, histY_condX);
+
+
+subplot(3, 1, 3); grid on; hold on;
+bar(y, pdfY_condX_sim, 'y');
+
+% f) Covariância entre X e Y
+rhoXY_sim = cov(X, Y) / sqrt(var(X) * var(Y))
 
