87c059706617565a2555fabd78ea7fc11c4269e3
RhenzoHideki <rhenzo.hsk@aluno.ifsc.edu.br>
2023-10-04 14:16:11 -0300

    .

--- Prova_3/prova_3.m
+++ Prova_3/prova_3.m
@@ -12,7 +12,7 @@ close all; clear all ; clc;
 
 N = 100000;
 X = 20*rand(1 ,N) - 10;
-dy = 0.002; y = -1.2 : dy : 1.2;
+
 
 for i = 1 : N
   Y(i) = rand()*(20 - abs(X(i))) ;
@@ -21,7 +21,6 @@ end
 figure; hold on ; grid on;
 scatter(X(1:5000),Y(1:5000));
 axis('square');
-dy = 0.002; y = -1.2 : dy : 1.2;
 
 ##(b) Determine Pr[X ≥ Y ].a
 
@@ -32,8 +31,7 @@ PrX_maior_igual_Y_teo = 1/6
 
 y = -25 : 0.5 : 25;
 
-histY = hist(Y, y);
-pdfY_sim = histY / trapz(y, histY);
+pdfY_sim = hist(Y, y) / (N);
 %pdfY_teo ;
 
 figure
@@ -42,7 +40,7 @@ bar(y, pdfY_sim, 'y');
 
 % d) CDF marginal de Y
 
-cdfY_sim = cumsum(histY) / N;
+cdfY_sim = cumsum(hist(Y, y)) / N;
 
 subplot(3, 1, 2); grid on; hold on;
 plot(y, cdfY_sim, 'g', 'LineWidth', 4);
@@ -51,13 +49,12 @@ plot(y, cdfY_sim, 'g', 'LineWidth', 4);
 % e) PDF condicional de Y dado que X = 5
 x0 = 5;
 Ycond = Y(abs(X - x0) < 0.001);
-pdfY_condX_sim = hist(Ycond , y)/(length(Ycond)* dy) ;
+pdfYcond_sim = hist(Ycond , y)/(length(Ycond)) ;
 
 
 subplot(3, 1, 3); grid on; hold on;
-bar(y, pdfY_condX_sim  , 'y');
-
+bar(y, pdfYcond_sim, 'y');
 
 % f) Covariância entre X e Y
 rhoXY_sim = cov(X, Y) / sqrt(var(X) * var(Y))
-rho
+
